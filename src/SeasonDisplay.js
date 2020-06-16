9c38637ad5d45adf81c23897fd86349f9a7741a2
S0f1eM <s.marchand56@gmail.com>
2020-06-16 14:18:55 +0200

    structuring app with class-based components

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SeasonDisplay extends React.Component  {
+
+	render() {
+		return (
+			<div>Season display</div>
+		)
+	}
+}
+
+
+ReactDOM.render(
+	<App />,
+	document.querySelector("#root")
+);
